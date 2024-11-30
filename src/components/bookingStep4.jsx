4c0e5cc6516f2683d2a48bb7408abaade207282e
Kavita Kumari <chauhan.kavita2248@gmail.com>
2024-11-30 23:34:28 +0530

    step4 done, some part still left

--- src/components/bookingStep4.jsx
+++ src/components/bookingStep4.jsx
@@ -16,8 +16,191 @@ import {
   Spinner,
 } from "reactstrap";
 import SvgIcons from "./SvgIcons";
+import { useEffect, useState } from "react";
+import {
+  createBooking,
+  getDetailsWithEmail,
+  sendOtpEmailForBooking,
+  verificationForBooking,
+} from "../http/http-calls";
+import { useDispatch, useSelector } from "react-redux";
+import { clearBooking } from "../redux/actions";
 
 const BookingStep4 = ({ onNext }) => {
+  const [buttonName, setButtonName] = useState("Check Email");
+  const [formFields, setFormFields] = useState({
+    email: "",
+    name: "",
+    phone: "",
+  });
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  const phoneRegex = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/;
+  const [totalValue, setTotalValue] = useState();
+  const [errors, setErrors] = useState({
+    email: "",
+    name: "",
+    phone: "",
+  });
+  const [isDirty, setIsDirty] = useState({
+    email: false,
+    name: false,
+    phone: false,
+  });
+  const agentFee = 10;
+  // console.log("isDirty>>", isDirty);
+  console.log("formFields>>", formFields);
+  // console.log("errors>>", errors);
+
+  const dispatch = useDispatch();
+
+  const storedBookingData = useSelector((state) => state.bookingDataReducer);
+  // console.log("storedBookingData>>>", storedBookingData);
+
+  useEffect(() => {
+    if (storedBookingData) {
+      const totalProductValue =
+        storedBookingData?.step2?.productValue + agentFee;
+      setTotalValue(totalProductValue);
+    }
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const updatedFormFields = { ...formFields };
+    updatedFormFields[name] = value;
+    setFormFields(updatedFormFields);
+    validateForm(updatedFormFields);
+  };
+
+  const handleBlur = (event) => {
+    const { name } = event?.target;
+    const updatedIsDirty = { ...isDirty };
+    updatedIsDirty[name] = true;
+    setIsDirty(updatedIsDirty);
+    validateForm(formFields);
+  };
+
+  const markAllDirty = () => {
+    return new Promise((resolve) => {
+      Object.keys(isDirty).forEach((each) => {
+        isDirty[each] = true;
+      });
+      setIsDirty(isDirty);
+      resolve();
+    });
+  };
+
+  const validateForm = (updatedFormFields) => {
+    const updatedErrors = {};
+    let isFormValid = true;
+
+    Object.keys(updatedFormFields).forEach((each) => {
+      switch (each) {
+        case "name":
+          if (!updatedFormFields?.name) {
+            updatedErrors.name = "*Required";
+            isFormValid = false;
+          }
+          break;
+
+        case "email":
+          if (!updatedFormFields?.email) {
+            updatedErrors.email = "*Required";
+            isFormValid = false;
+          } else if (!emailRegex.test(updatedFormFields?.email)) {
+            updatedErrors.email = "Invalid email!";
+            isFormValid = false;
+          }
+          break;
+
+        case "phone":
+          if (!updatedFormFields?.phone) {
+            updatedErrors.phone = "*Required";
+            isFormValid = false;
+          } else if (!phoneRegex.test(updatedFormFields?.phone)) {
+            updatedErrors.phone = "Invalid phone no.!";
+            isFormValid = false;
+          }
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    setErrors(updatedErrors); // Ensure errors are updated before returning isValid
+    return isFormValid;
+  };
+
+  const handleButtons = async (event, name) => {
+    if (name === "Check Email") {
+      try {
+        const payload = {
+          agentId: "63997eef2475d90ca5205bd2",
+          email: formFields.email,
+        };
+        const response = await getDetailsWithEmail(payload);
+        console.log("response >>", response);
+        if (response) {
+          setButtonName("Send OTP");
+        }
+      } catch (error) {
+        console(error);
+      }
+    } else if (name === "Send OTP") {
+      try {
+        const payload = {
+          agentId: "63997eef2475d90ca5205bd2",
+          email: formFields.email,
+        };
+        const response = await sendOtpEmailForBooking(payload);
+        console.log("response >>", response);
+        if (response) {
+          setButtonName("Send OTP");
+        }
+      } catch (error) {
+        console(error);
+      }
+    } else if (name === "Verify") {
+      if (formFields.otp) {
+        try {
+          const payload = {
+            agentId: "63997eef2475d90ca5205bd2",
+            email: formFields.email,
+            token: formFields.otp,
+          };
+          const response = await verificationForBooking(payload);
+          console.log("response >>", response);
+          if (response) {
+            setButtonName("Verified");
+          }
+        } catch (error) {
+          console(error);
+        }
+      }
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await markAllDirty();
+      const isValid = await validateForm(formFields);
+      console.log("isValid >>", isValid);
+      if (isValid) {
+        // const payload = {
+        //   // create payload here using redux store
+        // };
+        // const response = await createBooking(payload);
+        // if (response) {
+        //   dispatch(clearBooking());
+        // }
+        alert("sucessfull Booking");
+      }
+    } catch (error) {
+      console(error);
+    }
+  };
+
   const handlePreviousAndNext = (buttonName) => {
     if (buttonName === "previous") {
       onNext("3", "3");
@@ -36,33 +219,57 @@ const BookingStep4 = ({ onNext }) => {
               <div className="formGroup">
                 <Label>Email</Label>
                 <InputGroup className="InputWithButton">
-                  <Input placeholder="Enter" name="email" />
-                  {/* <InputGroupText>
-                    <span className="text-error">Not Verified</span>
-                  </InputGroupText> */}
+                  <Input
+                    type="email"
+                    placeholder="Enter your Email ID"
+                    name="email"
+                    onChange={(e) => handleChange(e)}
+                    onBlur={handleBlur}
+                  />
 
-                  {/* <InputGroupText>
-                    <Button color="success">Verified</Button>
-                  </InputGroupText> */}
+                  {buttonName === "Not Verified" && (
+                    <InputGroupText>
+                      <span className="text-error">Not Verified</span>
+                    </InputGroupText>
+                  )}
 
-                  {/* <InputGroupText>
-                    <Button
-                      color="primary"
-                    >
-                      Send OTP
-                    </Button>
-                  </InputGroupText> */}
+                  {buttonName === "Verified" && (
+                    <InputGroupText>
+                      <Button color="success">Verified</Button>
+                    </InputGroupText>
+                  )}
+
+                  {buttonName === "Send OTP" && (
+                    <InputGroupText>
+                      <Button
+                        color="primary"
+                        onClick={(e) => handleButtons(e, "Send OTP")}
+                      >
+                        Send OTP
+                      </Button>
+                    </InputGroupText>
+                  )}
 
                   {/* <InputGroupText>
                     <span className="text-success">Verified</span>
                   </InputGroupText> */}
-
-                  <InputGroupText>
-                    <Button color="link" className="p-0">
-                      Check Email
-                    </Button>
-                  </InputGroupText>
+                  {buttonName === "Check Email" && (
+                    <InputGroupText>
+                      <Button
+                        color="link"
+                        className="p-0"
+                        onClick={(e) => handleButtons(e, "Check Email")}
+                      >
+                        Check Email
+                      </Button>
+                    </InputGroupText>
+                  )}
                 </InputGroup>
+                {isDirty?.email ? (
+                  <p style={{ color: "red", fontSize: "15px" }}>
+                    {errors?.email}
+                  </p>
+                ) : null}
 
                 {/* Show timer count down when otp is sent */}
                 {/* <span className="resetOtpCount">
@@ -78,14 +285,21 @@ const BookingStep4 = ({ onNext }) => {
               <div className="formGroup">
                 <Label>Enter the code sent to your email</Label>
                 <InputGroup className="InputWithButton">
-                  <Input placeholder="Enter" name="token" />
+                  <Input
+                    placeholder="Enter the OTP"
+                    name="otp"
+                    onChange={handleChange}
+                  />
 
                   {/* show this when email is verified */}
-                  {/* <InputGroupText>
-                  <Button color="link" onClick={_verifyMail}>
-                    verify
-                  </Button>
-                </InputGroupText> */}
+                  <InputGroupText>
+                    <Button
+                      color="link"
+                      onClick={(e) => handleButtons(e, "Verify")}
+                    >
+                      verify
+                    </Button>
+                  </InputGroupText>
                 </InputGroup>
 
                 {/* Show this when otp is sent with email address */}
@@ -97,8 +311,17 @@ const BookingStep4 = ({ onNext }) => {
             <Col lg={6} md={6}>
               <FormGroup>
                 <Label>Name</Label>
-                <Input placeholder="Enter" name="name" />
+                <Input
+                  type="text"
+                  placeholder="Enter your Name"
+                  name="name"
+                  onChange={(e) => handleChange(e)}
+                  onBlur={handleBlur}
+                />
               </FormGroup>
+              {isDirty?.name ? (
+                <p style={{ color: "red", fontSize: "15px" }}>{errors?.name}</p>
+              ) : null}
             </Col>
             <Col lg={6} md={6}>
               <div className="formGroup">
@@ -109,8 +332,19 @@ const BookingStep4 = ({ onNext }) => {
                       <option>+1</option>
                     </Input>
                   </InputGroupText>
-                  <Input placeholder="Enter" name="phone" />
+                  <Input
+                    type="number"
+                    placeholder="Enter your Phone No."
+                    name="phone"
+                    onChange={(e) => handleChange(e)}
+                    onBlur={handleBlur}
+                  />
                 </InputGroup>
+                {isDirty?.phone ? (
+                  <p style={{ color: "red", fontSize: "15px" }}>
+                    {errors?.phone}
+                  </p>
+                ) : null}
               </div>
             </Col>
           </Row>
@@ -128,21 +362,21 @@ const BookingStep4 = ({ onNext }) => {
                   <span>Agent Fee:</span>
                   <span>
                     <FontAwesomeIcon icon={faDollarSign} />
-                    $50.33
+                    {storedBookingData?.step2?.productValue}
                   </span>
                 </li>
                 <li>
                   <span>Processing Fee:</span>
                   <span>
                     <FontAwesomeIcon icon={faDollarSign} />
-                    $1.76
+                    {agentFee}
                   </span>
                 </li>
                 <li>
                   <span>Total:</span>
                   <span>
                     <FontAwesomeIcon icon={faDollarSign} />
-                    $52.09
+                    {totalValue}
                   </span>
                 </li>
               </ul>
@@ -160,12 +394,20 @@ const BookingStep4 = ({ onNext }) => {
       </Card>
 
       <div className="tabAction">
-        <Button color="primary" outline onClick={() => handlePreviousAndNext("previous")}>
+        <Button
+          color="primary"
+          outline
+          onClick={() => handlePreviousAndNext("previous")}
+        >
           <SvgIcons type={"logArrowLeft"} />
           Previous
         </Button>
         <div>
-          <Button color="primary" className="ms-auto" onClick={() => handlePreviousAndNext("book")}>
+          <Button
+            color="primary"
+            className="ms-auto"
+            onClick={() => handleSubmit("book")}
+          >
             <i className="fa fa-spinner fa-spin mr-2" />
             Book
           </Button>
